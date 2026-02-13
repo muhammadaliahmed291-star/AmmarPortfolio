165526bb5c82ada2b53944d7bec20714973db69c
Ali Raza <razaworld72575@gmail.com>
2026-02-13 11:22:22 +0500

    image deleted

--- utils/images.js
+++ utils/images.js
@@ -3,10 +3,9 @@ import ammarImg from "../assets/images/ammar.jpeg";
 import digitalAgency from "../assets/images/digitalAgency.png"
 import e_comerence from "../assets/images/e_comerence.jpeg";
 import loanManager from "../assets/images/loanManager.png";
-import ridesharing from "../assets/images/ridesharing.jpg";
+
 
 export { ammarImg,
-    ridesharing,
     digitalAgency,
     e_comerence,
     loanManager
