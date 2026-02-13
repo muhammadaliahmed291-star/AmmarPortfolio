e7e7751c5d3b5259593f35ac105b71f356d98e08
muhammadaliahmed291-star <muhammadaliahmed291@gmail.com>
2026-02-12 22:28:29 -0800

    setting image

--- constants.tsx
+++ constants.tsx
@@ -10,12 +10,12 @@ export const PROJECTS: Project[] = [
     description: 'A Ride Sharing App is a mobile application that allows users to book rides easily from one location to another.',
     image: ridesharing, // local image
     tags: ['UX Design', 'Figma', 'Web & Mobile'],
-    figmaLink:'https://www.figma.com/proto/xqZAO333o6aEWLtieT8rlH/Ride-Sharing-App?node-id=109-2&t=QuI9CGWuh0A1gPGI-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=109%3A2'
+    figmaLink: 'https://www.figma.com/proto/xqZAO333o6aEWLtieT8rlH/Ride-Sharing-App?node-id=109-2&t=QuI9CGWuh0A1gPGI-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=109%3A2'
   },
   {
     id: '2',
-    title: 'MAP APP',
-    category: 'MAP Platform',
+    title: 'EcoSphere Analytics',
+    category: 'SaaS Platform',
     description: 'A dashboard design to visualize environmental data and carbon emissions trends clearly.',
     image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=80&w=1200', // online URL
     tags: ['Data Visualization', 'Web App', 'UI Design'],
@@ -46,7 +46,7 @@ export const PROJECTS: Project[] = [
     description: 'A portfolio site design to showcase agency projects, case studies, and services with clarity.',
     image: digitalAgency, // local image
     tags: ['Portfolio', 'Web Design', 'UI/UX'],
-    figmaLink:'https://www.figma.com/proto/N84WglRYfOGc4i5W3ibFK8/E-commerce-APP?node-id=3-26&p=f&t=wosNQgTznBJvRpoS-1'
+    figmaLink: 'https://www.figma.com/proto/N84WglRYfOGc4i5W3ibFK8/E-commerce-APP?node-id=3-26&p=f&t=wosNQgTznBJvRpoS-1'
   }
 ];
 
